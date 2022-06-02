af7e24e6f10e3bf2b5665404cfbf1bea167a0060
Paweł Szot <pawel.szot@beekeeper.io>
2022-06-02 09:04:23 +0200

    define modules for ogg and opus

---
+++ opus/opus_umbrella.h
@@ -0,0 +1,19 @@
+//
+//  opus_umbrella.h
+//  opus
+//
+//  Created by Pawel Szot on 02/06/2022.
+//
+
+#ifndef opus_umbrella_h
+#define opus_umbrella_h
+
+#include <opus/opus_defines.h>
+#include <opus/opus_multistream.h>
+#include <opus/opus_projection.h>
+#include <opus/opus_types.h>
+#include <opus/opus.h>
+#include <opus/opusenc.h>
+#include <opus/opusfile.h>
+
+#endif /* opus_umbrella_h */
